156cabe62c91f0cd08bf2c5012b01186b3a058d7
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-01-31 23:57:42 +0900

    fix: タイプミスの修正

--- components/notice-card.tsx
+++ components/notice-card.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 
-type CardProrps = {
+type CardProps = {
 	title: string;
 	description: string;
 	isUnread?: boolean; // 未読フラグ
 };
 
-export function Card({ title, description, isUnread = false }: CardProrps) {
+export function Card({ title, description, isUnread = false }: CardProps) {
 	return (
 		<div className="relative flex items-center bg-white w-[353px] h-[125px] p-4 mb-[40px] rounded-[30px] text-black">
 			{/* 未読バッジ（isUnreadがtrueの時だけ表示）*/}
