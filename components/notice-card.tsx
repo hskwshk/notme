c7aa3c3c5ee79758bbd9ace2f789da9a13f33cb9
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-01-31 21:22:18 +0900

    add: コンポーネントの繰り返し表示

--- components/notice-card.tsx
+++ components/notice-card.tsx
@@ -1,25 +1,19 @@
-// components/notice-card.tsx
-// type ButtonProps = {
-//   text: string;
-//   onClick: () => void;
-// };
-
-// export function Button({ text, onClick }: ButtonProps) {
-//   return (
-//     <button
-//       onClick={onClick}
-//       className="bg-blue-500 text-white px-4 py-2 rounded"
-//     >
-//       {text}
-//     </button>
-//   );
-// }
 import Image from "next/image";
 
-export function Card() {
+type CardProrps = {
+	title: string;
+	description: string;
+	isUnread?: boolean; // 未読フラグ
+};
+
+export function Card({ title, description, isUnread = false }: CardProrps) {
 	return (
-		<div className="relative bg-white w-[353px] h-[125px] p-4 rounded-[30px] text-black">
-			<div className="absolute top-[18px] right-[30px] bg-[#FEE123] w-[11px] h-[11px] rounded-full"></div>
+		<div className="relative flex items-center bg-white w-[353px] h-[125px] p-4 mb-[40px] rounded-[30px] text-black">
+			{/* 未読バッジ（isUnreadがtrueの時だけ表示）*/}
+			{isUnread && (
+				<div className="absolute top-[18px] right-[30px] bg-[#FEE123] w-[11px] h-[11px] rounded-full"></div>
+			)}
+
 			<div className="flex justify-center items-center">
 				<div className="bg-[#999] min-w-[47px] min-h-[47px] mr-[25px]  rounded-[50px]">
 					{/* <Image
@@ -30,10 +24,8 @@ export function Card() {
           /> */}
 				</div>
 				<div>
-					<h3 className="font-bold">テキストが入ります</h3>
-					<p>
-						テキストが入りますテキストが入りますテキストが入りますテキストが入ります
-					</p>
+					<h3 className="font-bold">{title}</h3>
+					<p>{description}</p>
 				</div>
 			</div>
 		</div>
