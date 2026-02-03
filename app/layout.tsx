935b8d84f8b3d0d7c75464355bb870e0afd1eaa7
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-02-03 12:14:43 +0900

    fix: とくぞうstyleの無効化

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
 
-import { BottomNav } from "@/components/bottom-nav";
-import { SiteHeader } from "@/components/site-header";
+// import { BottomNav } from "@/components/bottom-nav";
+// import { SiteHeader } from "@/components/site-header";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
+// const geistSans = Geist({
+// 	variable: "--font-geist-sans",
+// 	subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+// 	variable: "--font-geist-mono",
+// 	subsets: ["latin"],
+// });
 
-export const metadata: Metadata = {
-	title: "Next Tokuzou Kit",
-	description: "Next.js + Hono + Better Auth demo",
-};
+// export const metadata: Metadata = {
+// 	title: "Next Tokuzou Kit",
+// 	description: "Next.js + Hono + Better Auth demo",
+// };
 
 export default function RootLayout({
 	children,
@@ -28,13 +28,14 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-zinc-50 text-zinc-900 antialiased`}
+			// className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-zinc-50 text-zinc-900 antialiased`}
 			>
-				<SiteHeader />
-				<main className="mx-auto w-full max-w-5xl px-6 py-10 pb-24">
+				{/* <SiteHeader /> */}
+				{/* <main className="mx-auto w-full max-w-5xl px-6 py-10 pb-24">
 					{children}
-				</main>
-				<BottomNav />
+				</main> */}
+				{/* <BottomNav /> */}
+				<main className="px-[20px]">{children}</main>
 			</body>
 		</html>
 	);
