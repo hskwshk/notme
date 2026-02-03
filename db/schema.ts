426365d5686d80d22cca11c205a3d0caca8fbf6a
ShokiHosokawa <hosokawa.s.24kdgn@gmail.com>
2026-02-04 08:20:12 +0900

    fix: エラー修正

--- db/schema.ts
+++ db/schema.ts
@@ -13,7 +13,7 @@ import {
 export const user = pgTable("user", {
 	id: text("id").primaryKey(),
 	name: text("name").notNull(),
-	username: text("username").unique(),
+	username: text("username").unique().notNull(),
 	email: text("email").notNull().unique(),
 	emailVerified: boolean("email_verified").default(false).notNull(),
 	image: text("image"),
