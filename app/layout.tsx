d9a01ce8a21e3d5e16dad6e8cdec85d9cf795ff6
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-02-03 12:24:39 +0900

    add: bottomNavの追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,15 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-			// className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-zinc-50 text-zinc-900 antialiased`}
+				// className={`${geistSans.variable} ${geistMono.variable} min-h-screen bg-zinc-50 text-zinc-900 antialiased`}
+				className="px-[20px] pt-[60px]"
 			>
 				{/* <SiteHeader /> */}
 				{/* <main className="mx-auto w-full max-w-5xl px-6 py-10 pb-24">
 					{children}
 				</main> */}
 				{/* <BottomNav /> */}
-				<main className="px-[20px]">{children}</main>
+				<main>{children}</main>
 			</body>
 		</html>
 	);
