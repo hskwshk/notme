682ceb156e979c7d36f88799f15f317136df0396
ShokiHosokawa <hosokawa.s.24kdgn@gmail.com>
2026-02-03 20:39:57 +0900

    fix: 通知画面修復

--- app/notice/page.tsx
+++ app/notice/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-// import { group } from "console";
+import { BottomNav } from "@/components/bottom-nav";
+import { Card } from "@/components/notice-card";
+import { Top } from "@/components/top";
+
 // import Image from "next/image";
 
 // トップのテストデータ
@@ -60,9 +63,30 @@ export default function Notice() {
 	console.log("Notice Data:", noticesByDate);
 
 	return (
-		<div>
-			<h1>{pageName}</h1>
-			<p>Check console for data.</p>
-		</div>
+		<main className="my-[70px]">
+			<Top name={pageName} />
+
+			<div className="flex flex-col items-center">
+				{noticesByDate.map((group) => (
+					<section key={group.date}>
+						<h2 className="mb-[23px] text-[20px] font-bold text-black">
+							{group.date}
+						</h2>
+						<div>
+							{group.notices.map((notice) => (
+								<Card
+									key={notice.id}
+									title={notice.title}
+									description={notice.description}
+									isUnread={notice.isUnread}
+								/>
+							))}
+						</div>
+					</section>
+				))}
+			</div>
+
+			<BottomNav />
+		</main>
 	);
 }
