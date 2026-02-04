dd264c09e3889ce3db49641aa5c3b51907824105
ShokiHosokawa <hosokawa.s.24kdgn@gmail.com>
2026-02-04 15:48:17 +0900

    fix: アクセス権限修正

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,16 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { username } from "better-auth/plugins";
 import * as schema from "@/db/schema";
 import { db } from "@/lib/db";
 
 export const auth = betterAuth({
 	baseURL: process.env.BETTER_AUTH_URL,
+	trustedOrigins: [
+		"http://localhost:3000",
+		"http://127.0.0.1:3000",
+		"http://10.96.1.64:3000",
+	],
 	database: drizzleAdapter(db, {
 		provider: "pg",
 		schema,
@@ -23,4 +29,5 @@ export const auth = betterAuth({
 			},
 		},
 	},
+	plugins: [username()],
 });
