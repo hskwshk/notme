b9d97ca6d00d27ab1caf781b978b5e488d40fd5d
wkbys <wakabayashi.m.24kdgn@gmail.com>
2026-02-05 12:06:25 +0900

    fix: 不要な部分をコメントアウト

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
 
 import { BottomNav } from "@/components/bottom-nav";
 // import { SiteHeader } from "@/components/site-header";
